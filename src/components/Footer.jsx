89532b386e14b486bbf5e69dca4eb90573724d78
EusebioCO296 <eusebiocaballero7@gmail.com>
2026-08-01 17:18:58 -0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2026 My Website - Eusebio. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Students.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+
+function Students() {
+  return (
+    <div>
+      <header>Welcome</header>
+      <h1>Students Page</h1>
+      <footer><Footer/></footer>  
+    </div>
+  );
+}
+
+export default Students;
